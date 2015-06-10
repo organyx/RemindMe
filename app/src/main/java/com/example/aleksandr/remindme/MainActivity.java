a0a5ae652e4c8b3878a84210b975a0442b8bedf6
Aleks <aleksandr.beliun@gmail.com>
2015-06-10 14:59:53 +0300

    Main Activity

---
+++ app/src/main/java/com/example/aleksandr/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.aleksandr.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Aleksandr on 10/06/2015.
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acivity_main);
+    }
+}
